def53a40f330a7ef32cd6a9c656cf65897cdb835
Vlad Kozlovskiy <--global>
2025-03-18 12:13:55 +0300

    fixing bag

--- 02-blocing-event-loop/while.js
+++ 02-blocing-event-loop/while.js
@@ -4,12 +4,5 @@ setTimeout(() => (isRunning = false), 10);
 process.nextTick(() => console.log('NextTick'));
 
 while (isRunning) {
-    console.log('While lo1op is running');
+    console.log('While lo12op is running');
 }
-
-
-ads
-DataTransfersd
-a
-
-"asdasdasd"
